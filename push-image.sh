cbd9a21a16e046f8c03b9ec388dace2e5d0e9915
Armand Janse van Rensburg <armandjvr1991@gmail.com>
2024-09-18 06:12:22 +0400

    updated files

--- push-image.sh
+++ push-image.sh
@@ -4,10 +4,9 @@
 set -eu
 
 # login to your docker hub account
-cat ~/my_password.txt | sudo docker login --username aricloud1 --password-stdin
+docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 
 # use docker tag command to give the image a new name
-sudo docker tag techmax-as aricloud1/techmax-as
-
+docker tag $IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
 # push the image to your docker hub repository
-sudo docker push aricloud1/techmax-as
+docker push $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
