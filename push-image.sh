2f97d1a62a54c7f2ba1bf6f0ebb7042202fde3aa
Armand Janse van Rensburg <armandjvr1991@gmail.com>
2024-09-19 15:53:31 +0400

    git update

--- push-image.sh
+++ push-image.sh
@@ -8,5 +8,6 @@ docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 
 # use docker tag command to give the image a new name
 docker tag $IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
+
 # push the image to your docker hub repository
 docker push $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
